3937909937ff0d4fb8244d37954f6ef456a22d0f
cherepanov92 <Killimo513>
2019-09-11 21:53:27 +0500

    create models and admin (Allergen, Dish)

--- bistro/app/admin.py
+++ bistro/app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from app.models import *
 
-# Register your models here.
+
+class AllergenAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class DishAdmin(admin.ModelAdmin):
+    filter_horizontal = ('allergen',)
+    list_display = ('name',
+                    'calorie',
+                    'category',
+                    'price',
+                    'picture')
+
+
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Allergen, AllergenAdmin)
--- bistro/app/models.py
+++ bistro/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Allergen(models.Model):
+    name = models.CharField(max_length=100, verbose_name=u'Название')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Dish(models.Model):
+    # Категории
+    CATEGORY_CHOICES = (
+        (None, 'Без категории'),
+        (0, 'Закуски'),
+        (1, 'Горячие блюда'),
+        (2, 'Десерты'),
+        (3, 'Напитки'),
+        (4, 'Алкогольные напитки')
+    )
+
+    name = models.CharField(max_length=100, verbose_name='Название')
+    calorie = models.PositiveSmallIntegerField(verbose_name='Калорийность')
+    allergen = models.ManyToManyField(Allergen, related_name='dish', verbose_name='Аллергены', blank=True)
+    category = models.PositiveSmallIntegerField(choices=CATEGORY_CHOICES, null=True, blank=True)
+    price = models.PositiveSmallIntegerField(verbose_name='Цена')
+    picture = models.ImageField(upload_to='static/images/menu/', default='static/images/menu/no_img.jpg')
+
+    def __str__(self):
+        return f"{self.name} - {self.price}$"
